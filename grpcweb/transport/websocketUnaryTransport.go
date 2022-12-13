1289d5e1fddd6970f1be5be3f40ad47a2b3da41d
jbhusson <jonah@elixxir.io>
2022-12-13 11:25:05 -0500

    Add websocketUnaryTransport

---
+++ grpcweb/transport/websocketUnaryTransport.go
@@ -0,0 +1,252 @@
+package transport
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/tls"
+	"crypto/x509"
+	"encoding/pem"
+	"git.xx.network/elixxir/grpc-web-go-client/grpcweb/parser"
+	"github.com/pkg/errors"
+	"golang.org/x/net/context"
+	"io"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"net/url"
+	"nhooyr.io/websocket"
+	"strings"
+	"sync"
+)
+
+// webSocketTransport is a stream transport implementation.
+//
+// Currently, gRPC-Web specification does not support client streaming. (https://github.com/improbable-eng/grpc-web#client-side-streaming)
+// webSocketTransport supports improbable-eng/grpc-web's own implementation.
+//
+// spec: https://github.com/grpc/grpc/blob/master/doc/PROTOCOL-WEB.md
+type webSocketUnaryTransport struct {
+	host     string
+	endpoint string
+
+	conn *websocket.Conn
+
+	once    sync.Once
+	resOnce sync.Once
+
+	closed bool
+
+	writeMu sync.Mutex
+
+	reqHeader, header, trailer http.Header
+}
+
+func (t *webSocketUnaryTransport) Header() http.Header {
+	return t.reqHeader
+}
+
+func (t *webSocketUnaryTransport) GetRespHeader() (http.Header, error) {
+	return t.header, nil
+}
+
+func (t *webSocketUnaryTransport) Trailer() http.Header {
+	return t.trailer
+}
+
+func (t *webSocketUnaryTransport) SetRequestHeader(h http.Header) {
+	t.reqHeader = h
+}
+
+func (t *webSocketUnaryTransport) Send(ctx context.Context, _, _ string, body io.Reader) (http.Header, []byte, error) {
+	if t.closed {
+		return nil, nil, io.EOF
+	}
+
+	var err error
+	t.once.Do(func() {
+		h := t.reqHeader
+		if h == nil {
+			h = make(http.Header)
+		}
+		h.Set("content-type", "application/grpc-web+proto")
+		h.Set("x-grpc-web", "1")
+		var b bytes.Buffer
+		h.Write(&b)
+
+		t.writeMessage(int(websocket.MessageBinary), b.Bytes())
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var b bytes.Buffer
+	b.Write([]byte{0x00})
+	_, err = io.Copy(&b, body)
+	if err != nil {
+		return nil, nil, errors.Wrap(err, "failed to read request body")
+	}
+
+	rc, err := t.Receive(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	h, err := t.GetRespHeader()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	msgH, err := parser.ParseResponseHeader(rc)
+	if err != nil {
+		return nil, nil, err
+	}
+	msg, err := parser.ParseLengthPrefixedMessage(rc, msgH.ContentLength)
+
+	return h, msg, t.writeMessage(int(websocket.MessageBinary), b.Bytes())
+}
+
+func (t *webSocketUnaryTransport) Receive(context.Context) (_ io.ReadCloser, err error) {
+	if t.closed {
+		return nil, io.EOF
+	}
+
+	defer func() {
+		if err == nil {
+			return
+		}
+
+		if berr, ok := errors.Cause(err).(*net.OpError); ok && !berr.Temporary() {
+			err = io.EOF
+		}
+	}()
+
+	// skip response header
+	t.resOnce.Do(func() {
+		ctx := context.Background()
+		_, _, err = t.conn.Reader(ctx)
+		if err != nil {
+			err = errors.Wrap(err, "failed to read response header")
+			return
+		}
+
+		_, msg, err := t.conn.Reader(ctx)
+		if err != nil {
+			err = errors.Wrap(err, "failed to read response header")
+			return
+		}
+
+		h := make(http.Header)
+		s := bufio.NewScanner(msg)
+		for s.Scan() {
+			t := s.Text()
+			i := strings.Index(t, ": ")
+			if i == -1 {
+				continue
+			}
+			k := strings.ToLower(t[:i])
+			h.Add(k, t[i+2:])
+		}
+		t.header = h
+	})
+
+	var buf bytes.Buffer
+	var b []byte
+
+	_, b, err = t.conn.Read(context.Background())
+	if err != nil {
+		if cerr, ok := err.(*websocket.CloseError); ok {
+			if cerr.Code == websocket.StatusNormalClosure {
+				return nil, io.EOF
+			}
+			if cerr.Code == websocket.StatusAbnormalClosure {
+				return nil, io.ErrUnexpectedEOF
+			}
+		}
+		err = errors.Wrap(err, "failed to read response body")
+		return
+	}
+	buf.Write(b)
+
+	var r io.Reader
+	_, r, err = t.conn.Reader(context.Background())
+	if err != nil {
+		return
+	}
+
+	res := ioutil.NopCloser(io.MultiReader(&buf, r))
+
+	by, err := ioutil.ReadAll(res)
+	if err != nil {
+		panic(err)
+	}
+
+	res = ioutil.NopCloser(bytes.NewReader(by))
+
+	return res, nil
+}
+
+func (t *webSocketUnaryTransport) CloseSend() error {
+	// 0x01 means the finish send frame.
+	// ref. transports/websocket/websocket.ts
+	t.writeMessage(int(websocket.MessageBinary), []byte{0x01})
+	return nil
+}
+
+func (t *webSocketUnaryTransport) Close() error {
+	// Send the close message.
+
+	err := t.conn.Close(websocket.StatusNormalClosure, "")
+	if err != nil {
+		return err
+	}
+	t.closed = true
+	// Close the WebSocket connection.
+	return nil
+}
+
+func (t *webSocketUnaryTransport) writeMessage(msg int, b []byte) error {
+	t.writeMu.Lock()
+	defer t.writeMu.Unlock()
+	return t.conn.Write(context.Background(), websocket.MessageType(msg), b)
+}
+
+var NewWSUT = func(host, endpoint string, opts *ConnectOptions) (UnaryTransport, error) {
+	// TODO: WebSocket over TLS support.
+	h := http.Header{}
+	h.Set("Sec-WebSocket-Protocol", "grpc-websockets")
+	var conn *websocket.Conn
+	dialer := &websocket.DialOptions{}
+	dialer.HTTPClient = http.DefaultClient
+	scheme := "ws"
+	if opts.WithTLS {
+		scheme = "wss"
+		certPool := x509.NewCertPool()
+		decoded, _ := pem.Decode(opts.TLSCertificate)
+		if decoded == nil {
+			panic("failed to decode cert")
+		}
+		cert, err := x509.ParseCertificate(decoded.Bytes)
+		if err != nil {
+			panic(err)
+		}
+		certPool.AddCert(cert)
+		tlsConf := &tls.Config{RootCAs: certPool, ServerName: cert.DNSNames[0]}
+		tlsConf.InsecureSkipVerify = opts.TlsInsecureSkipVerify
+
+		dialer.HTTPClient.Transport = &http.Transport{
+			TLSClientConfig: tlsConf,
+		}
+
+	}
+	u := url.URL{Scheme: scheme, Host: host, Path: endpoint}
+	conn, _, err := websocket.Dial(context.Background(), u.String(), dialer)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to dial to '%s'", u.String())
+	}
+
+	return &webSocketUnaryTransport{
+		host:     host,
+		endpoint: endpoint,
+		conn:     conn,
+	}, nil
+}
